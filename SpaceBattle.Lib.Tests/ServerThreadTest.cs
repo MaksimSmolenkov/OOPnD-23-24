f3fdd5490096f6688cec207826afbd15cd2424c7
Maksim_Smolenkov <makssmolenkovmail.ru@gmail.com>
2024-03-06 07:16:18 +0000

    Add server thread

---
+++ SpaceBattle.Lib.Tests/ServerThreadTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Concurrent;
+using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
+
+public class ServerThreadTest
+{
+    public ServerThreadTest()
+    {
+
+        new InitScopeBasedIoCImplementationCommand().Execute();
+
+        IoC.Resolve<ICommand>("IoC.Register",
+            "Hard Stop The Thread",
+            (object[] args) =>
+            {
+                var thread = (ServerThread)args[0];
+                var action = (Action)args[1];
+                return new ActionCommand(
+                    () =>
+                    {
+                        new HardStopCommand(thread).Execute();
+                        new ActionCommand(action).Execute();
+                    }
+                );
+            }
+        ).Execute();
+
+    }
+    [Fact]
+    public void HardStopShouldStopServerThread()
+    {
+        var q = new BlockingCollection<ICommand>(10);
+
+        var st = new ServerThread(q);
+
+        var command = new Mock<ICommand>();
+        command.Setup(m => m.Execute()).Verifiable();
+
+        q.Add(command.Object);
+        var mre = new ManualResetEvent(false);
+        q.Add(IoC.Resolve<ICommand>("Hard Stop The Thread", st, () => { mre.Set(); }));
+        q.Add(command.Object);
+
+        st.Start();
+        mre.WaitOne();
+
+        Assert.Single(q);
+        command.Verify(m => m.Execute(), Times.Once);
+    }
+}
+
+public class ActionCommand : ICommand
+{
+    private readonly Action _action;
+    public ActionCommand(Action action)
+    {
+        _action = action;
+    }
+
+    public void Execute()
+    {
+        _action();
+    }
+}
