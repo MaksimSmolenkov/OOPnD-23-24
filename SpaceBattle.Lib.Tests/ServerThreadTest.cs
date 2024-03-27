b9e253ca1c998e5fb9ce235aac7e0913af807730
Maksim_Smolenkov <makssmolenkovmail.ru@gmail.com>
2024-03-27 09:06:22 +0000

    ST and STT

--- SpaceBattle.Lib.Tests/ServerThreadTest.cs
+++ SpaceBattle.Lib.Tests/ServerThreadTest.cs
@@ -1,13 +1,14 @@
-﻿using System.Collections.Concurrent;
+﻿using Moq;
+
+using System.Collections.Concurrent;
+
 using Hwdtech;
 using Hwdtech.Ioc;
-using Moq;
 using Spacebattle;
 
 public class ServerThreadTest
 {
-    public ServerThreadTest()
-    {
+    public ServerThreadTest() {
 
         new InitScopeBasedIoCImplementationCommand().Execute();
 
@@ -15,15 +16,13 @@ public class ServerThreadTest
             IoC.Resolve<object>("Scopes.New",
                 IoC.Resolve<object>("Scopes.Root")));
 
-        IoC.Resolve<ICommand>("IoC.Register",
+        IoC.Resolve<ICommand>("IoC.Register", 
             "Hard Stop The Thread",
-            (object[] args) =>
-            {
+            (object[] args) => {
                 var thread = (ServerThread)args[0];
-                var action = (Action)args[1];
-                return new ActionCommand(
-                    () =>
-                    {
+                var action = (Action)args[1]; 
+                return new ActionCommand (
+                    () => {
                         new ServerThread.HardStopCommand(thread).Execute();
                         new ActionCommand(action).Execute();
                     }
@@ -35,41 +34,42 @@ public class ServerThreadTest
     [Fact]
     public void HardStopShouldStopServerThread()
     {
+
         var setScopeCommand = IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set",
             IoC.Resolve<object>("Scopes.New",
                 IoC.Resolve<object>("Scopes.Current")));
 
         var mre = new ManualResetEvent(false);
-
+        
         var q = new BlockingCollection<ICommand>(10);
 
         var HandleCommand = new Mock<ICommand>();
         HandleCommand.Setup(m => m.Execute()).Verifiable();
 
-        var setRegisterCommand = IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ExceptionHandler.Handle", (object[] args) => HandleCommand.Object);
+        var setRegisterCommand = IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ExceptionHandler.Handle", (object[] args ) => HandleCommand.Object);
 
         q.Add(setScopeCommand);
         q.Add(setRegisterCommand);
 
         var command = new Mock<ICommand>();
         command.Setup(m => m.Execute()).Verifiable();
-
+        
         var st = new ServerThread(q);
-        var hs = IoC.Resolve<ICommand>("Hard Stop The Thread", st, () => { mre.Set(); });
+        var hs = IoC.Resolve<ICommand>("Hard Stop The Thread", st, ()=> {mre.Set();});
         q.Add(command.Object);
         q.Add(hs);
         q.Add(command.Object);
-
+        
         st.Start();
-
+        
         Assert.Single(q);
     }
 }
 
-public class ActionCommand : ICommand
+public class ActionCommand: ICommand 
 {
     private readonly Action _action;
-    public ActionCommand(Action action)
+    public ActionCommand(Action action) 
     {
         _action = action;
     }
