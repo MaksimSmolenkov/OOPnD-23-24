fb0e374d68ed406c48690e706647a5859e61486a
MaksimSmolenkov <125339609+MaksimSmolenkov@users.noreply.github.com>
2024-05-15 16:09:41 +0600

    Update StartMoveCommand.cs

--- SpaceBattle.Lib/StartMoveCommand.cs
+++ SpaceBattle.Lib/StartMoveCommand.cs
@@ -12,7 +12,7 @@ public class StartMoveCommand : ICommand
 
     public void Execute()
     {
-        _obj.Dict.ToList().ForEach(o => IoC.Resolve<ICommand>("Target.Register", _obj.UObject, o.Key, o.Value).Execute()); //
+        _obj.Dict.ToList().ForEach(o => IoC.Resolve<ICommand>("Target.Register", _obj.UObject, o.Key, o.Value).Execute());
         var mCommand = IoC.Resolve<ICommand>("Moving.Commands", _obj.UObject);
         var injectable = IoC.Resolve<ICommand>("Commands.Injectable", mCommand);
         IoC.Resolve<ICommand>("Target.Register", _obj.UObject, "Moving.Commands", mCommand).Execute();
