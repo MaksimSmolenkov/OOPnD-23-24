0c1fa6d1c523cb783e69002634627d2f43588301
Maksim_Smolenkov <makssmolenkovmail.ru@gmail.com>
2024-05-24 05:55:58 +0000

    fix .net version

---
+++ SpaceBattle.Lib/MacroCommand.cs
@@ -0,0 +1,18 @@
+﻿using Command;
+
+namespace Spacebattle;
+
+public class MacroCommand : ICommand
+{
+    private readonly IList<ICommand> list;
+
+    public MacroCommand(IList<ICommand> list)
+    {
+        this.list = list;
+    }
+
+    public void Execute()
+    {
+        list.ToList().ForEach(item => { item.Execute(); });
+    }
+}
---
+++ SpaceBattle.Lib/MacroCommandStrategy.cs
@@ -0,0 +1,19 @@
+﻿using Hwdtech;
+
+namespace Spacebattle;
+
+public class MacroCommandStrategy : IStrategy
+{
+    public object Strategy(params object[] args)
+    {
+        var key = (string)args[0];
+        var obj = (IUObject)args[1];
+
+        var dependencies = IoC.Resolve<IList<string>>("SpaceBattle.Operation." + key);
+
+        var resolvedDependencies = dependencies.Select(d => IoC.Resolve<Command.ICommand>(d, obj)).ToList();
+        IList<Command.ICommand> list = resolvedDependencies;
+
+        return new MacroCommand(list);
+    }
+}
