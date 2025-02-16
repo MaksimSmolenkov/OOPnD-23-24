0f494b78d27f502afcd3eb92fad404211e2a5d8b
MaksimSmolenkov <makssmolenkovmail.ru@gmaill.com>
2025-02-16 17:20:40 +0600

    Add ShootCommand

---
+++ SpaceBattle.Lib.Tests/ShootCommandTest.cs
@@ -0,0 +1,59 @@
+﻿using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
+using Spacebattle;
+
+namespace SpaceBattle.Lib.Test
+{
+    public class ShootCommandTests
+    {
+        public ShootCommandTests()
+        {
+            new InitScopeBasedIoCImplementationCommand().Execute();
+            IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+        }
+
+        [Fact]
+        public void ShootCommand_CreatesProjectileWithCorrectProperties()
+        {
+            var shooterMock = new Mock<IShootable>();
+            var gameFieldMock = new Mock<IUObject>();
+            var projectileMock = new Mock<IUObject>();
+
+            var projectileType = "Torpedo";
+
+            shooterMock.SetupGet(s => s.ProjectileType).Returns(projectileType);
+            shooterMock.SetupGet(s => s.Pos).Returns((vectr.Vector)It.IsAny<object>());
+            shooterMock.SetupGet(s => s.Velocity).Returns((vectr.Vector)It.IsAny<object>());
+
+            Func<string, IUObject> projectileFactory = (type) =>
+            {
+                Assert.Equal(projectileType, type);
+                return projectileMock.Object;
+            };
+
+            Action<IUObject, IShootable> projectileConfigurator = (projectile, shooter) =>
+            {
+                projectile.SetProperty("Location", shooter.Pos);
+                projectile.SetProperty("Velocity", shooter.Velocity);
+            };
+
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ShootCommand.ProjectileFactory", (object[] args) => projectileFactory).Execute();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ShootCommand.ProjectileConfigurator", (object[] args) => projectileConfigurator).Execute();
+
+            var shootCommand = new ShootCommand(
+                shooterMock.Object,
+                gameFieldMock.Object,
+                IoC.Resolve<Func<string, IUObject>>("ShootCommand.ProjectileFactory"),
+                IoC.Resolve<Action<IUObject, IShootable>>("ShootCommand.ProjectileConfigurator")
+            );
+
+            shootCommand.Execute();
+
+            projectileMock.Verify(p => p.SetProperty("Location", It.IsAny<object>()), Times.Once);
+            projectileMock.Verify(p => p.SetProperty("Velocity", It.IsAny<object>()), Times.Once);
+
+            gameFieldMock.Verify(g => g.SetProperty("Projectiles", It.IsAny<object>()), Times.Once);
+        }
+    }
+}
---
+++ SpaceBattle.Lib/ShootCommand.cs
@@ -0,0 +1,54 @@
+﻿using Command;
+using Spacebattle;
+
+namespace SpaceBattle.Lib
+{
+    public class AddProjectileToFieldCommand : ICommand
+    {
+        private readonly IUObject _gameField;
+        private readonly IUObject _projectile;
+
+        public AddProjectileToFieldCommand(IUObject gameField, IUObject projectile)
+        {
+            _gameField = gameField;
+            _projectile = projectile;
+        }
+
+        public void Execute()
+        {
+            var projectiles = _gameField.GetProperty("Projectiles") as List<IUObject> ?? new List<IUObject>();
+            projectiles.Add(_projectile);
+            _gameField.SetProperty("Projectiles", projectiles);
+        }
+    }
+
+    public class ShootCommand : ICommand
+    {
+        private readonly IShootable _shooter;
+        private readonly IUObject _gameField;
+        private readonly Func<string, IUObject> _projectileFactory;
+        private readonly Action<IUObject, IShootable> _projectileConfigurator;
+
+        public ShootCommand(IShootable shooter, IUObject gameField, Func<string, IUObject> projectileFactory, Action<IUObject, IShootable> projectileConfigurator)
+        {
+            _shooter = shooter;
+            _gameField = gameField;
+            _projectileFactory = projectileFactory;
+            _projectileConfigurator = projectileConfigurator;
+        }
+
+        public void Execute()
+        {
+            if (string.IsNullOrEmpty(_shooter.ProjectileType))
+            {
+                throw new InvalidOperationException("Отсутствует тип снаряда.");
+            }
+
+            var projectile = _projectileFactory.Invoke(_shooter.ProjectileType);
+
+            _projectileConfigurator.Invoke(projectile, _shooter);
+
+            new AddProjectileToFieldCommand(_gameField, projectile).Execute();
+        }
+    }
+}
