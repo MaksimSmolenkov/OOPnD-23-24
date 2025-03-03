ae26429577404a73828a88e746ecc9fdc0c05858
MaksimSmolenkov <makssmolenkovmail.ru@gmaill.com>
2025-03-03 19:37:36 +0600

    move template to lib

--- SpaceBattle.Lib/CodeStringBuilder.cs
+++ SpaceBattle.Lib/CodeStringBuilder.cs
@@ -1,9 +1,34 @@
-﻿using Hwdtech;
-using Scriban;
+﻿using Scriban;
 
 namespace SpaceBattle.Lib;
+
 public class CodeStringAdapterBuilder : IBuilder
 {
+    private const string TemplateText = @"using System;
+public class {{name }}
+{
+    private object obj;
+    {{ for property in properties }}
+    private {{ property.type }} {{ property.name }}{
+    {{if property.set}}
+    set
+    {
+        Hwdtech.IoC.Resolve<SpaceBattle.Lib.ICommand>(""{{property.name}}.Set"", obj, value).Execute();
+    }
+    {{end}}  
+    get
+    {{if property.get}}
+    {
+        return Hwdtech.IoC.Resolve<{{property.type}}>(""{{property.name}}.Get"", obj);
+    } 
+    {{ end }}
+    }
+    {{ end }}
+    public {{ name }}(object obj)
+    {
+        this.obj = obj;
+    }
+}";
 
     private readonly string _className;
     private readonly List<object> _members;
@@ -12,8 +37,8 @@ public class CodeStringAdapterBuilder : IBuilder
     {
         _className = className;
         _members = new List<object>();
-
     }
+
     public CodeStringAdapterBuilder AddMember(object property)
     {
         _members.Add(property);
@@ -22,8 +47,7 @@ public class CodeStringAdapterBuilder : IBuilder
 
     public string Build()
     {
-        var templateText = IoC.Resolve<string>("Template");
-        var template = Template.Parse(templateText);
+        var template = Template.Parse(TemplateText);
         var result = template.Render(new { name = _className, properties = _members.ToArray() });
         return result;
     }
