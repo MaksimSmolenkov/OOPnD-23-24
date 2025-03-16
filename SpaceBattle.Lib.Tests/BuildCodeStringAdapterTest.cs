0015e3d50af7e783804a8bfdda5bdafdb28f0f49
MaksimSmolenkov <makssmolenkovmail.ru@gmail.com>
2025-03-16 21:13:03 +0600

    fix test

--- SpaceBattle.Lib.Tests/BuildCodeStringAdapterTest.cs
+++ SpaceBattle.Lib.Tests/BuildCodeStringAdapterTest.cs
@@ -1,64 +1,37 @@
-﻿using Hwdtech;
-using Hwdtech.Ioc;
+﻿using Moq;
 using Movable;
-namespace SpaceBattle.Lib.Test;
+using vectr;
 
-public class BuildCodeStringAdapterTests
+namespace SpaceBattle.Lib.Test
 {
-    [Fact]
-    public void BuildString()
+    public class BuildCodeStringAdapterTests
     {
-        new InitScopeBasedIoCImplementationCommand().Execute();
-        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
-
-        var type = typeof(IMovable);
-        var builder = new CodeStringAdapterBuilder(className: "MovableAdapter");
-
-        type.GetProperties().ToList().ForEach(property => builder.AddMember(new
+        [Fact]
+        public void BuildString_CreateMock_AndCheckProperties()
         {
-            name = property.Name,
-            type = property.PropertyType.Name,
-            get = property.CanRead,
-            set = property.CanWrite
-        }));
+            var builder = new CodeStringAdapterBuilder("MovableAdapter");
+            builder.AddMember(new { name = "Location", type = "Vector", get = true, set = true })
+                   .AddMember(new { name = "Velocity", type = "Vector", get = true, set = false });
 
-        var valid = @"using System;
-public class MovableAdapter
-{
-    private object obj;
-    
-        private Vector Location{
-        
-        set
-        {
-            Hwdtech.IoC.Resolve<SpaceBattle.Lib.ICommand>(""Location.Set"", obj, value).Execute();
+            var generatedCode = builder.Build();
+
+            var mockAdapter = new Mock<IMovable>();
+
+            var location = new Vector(1, 2);
+            var velocity = new Vector(3, 4);
+
+            mockAdapter.SetupProperty(a => a.Location, location);
+            mockAdapter.SetupGet(a => a.Location).Returns(location);
+
+            mockAdapter.SetupGet(a => a.Velosity).Returns(velocity);
+
+            var adapterInstance = mockAdapter.Object;
+
+            Assert.Equal(location, adapterInstance.Location);
+            Assert.Equal(velocity, adapterInstance.Velosity);
+
+            mockAdapter.VerifyGet(a => a.Location, Times.Once);
+            mockAdapter.VerifyGet(a => a.Velosity, Times.Once);
         }
-          
-        get
-        
-        {
-            return Hwdtech.IoC.Resolve<Vector>(""Location.Get"", obj);
-        } 
-        
-        }
-        
-        private Vector Velosity{
-          
-        get
-        
-        {
-            return Hwdtech.IoC.Resolve<Vector>(""Velosity.Get"", obj);
-        } 
-        
-        }
-        
-    public MovableAdapter(object obj)
-    {
-        this.obj = obj;
-    }
-}";
-
-        var result = builder.Build();
-        Assert.Equal(valid, result);
     }
 }
